87c4ee44ae166254243736d76d2a8078b46e7052
Ashish Khati <khatiashish2442@gmail.com>
2023-03-20 01:21:13 +0530

    dd

--- server/DB/conn.js
+++ server/DB/conn.js
@@ -2,9 +2,9 @@ const mongoose= require('mongoose')
 const dotenv=require('dotenv')
 dotenv.config({path:'./config.env'})
 const DB=process.env.DATABASE;
-mongoose.connect(DB,{
+mongoose.connect(DB ,{
     useNewUrlParser:true,
-    useUnifiedTopology: true
+    
 }).then(()=>{
     console.log("Connection successfull")
 }).catch((err)=>console.log('no connection',err))
